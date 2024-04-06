9eba2d6ab242bc0485f8421393aff524029fad51
PabloMonjiDev <pablo20044814@gmail.com>
2024-04-06 17:52:18 +0200

    Primer Commit

---
+++ src/main/java/com/pablodelacruz/coachify/CoachifyApplication.java
@@ -0,0 +1,13 @@
+package com.pablodelacruz.coachify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoachifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoachifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pablodelacruz/coachify/CoachifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pablodelacruz.coachify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoachifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
