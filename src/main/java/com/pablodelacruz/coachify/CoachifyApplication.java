76f4d908f06dc23c2254b973c00cf37ab600e12f
PabloMonjiDev <pablo20044814@gmail.com>
2024-04-10 19:35:36 +0200

    He excluido temporalmente el security

--- src/main/java/com/pablodelacruz/coachify/CoachifyApplication.java
+++ src/main/java/com/pablodelacruz/coachify/CoachifyApplication.java
@@ -2,8 +2,9 @@ package com.pablodelacruz.coachify;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class CoachifyApplication {
 
 	public static void main(String[] args) {
