eb0eb3ee7dfb0f0858c3702c79e20b5929f84365
PabloMonjiDev <pablo20044814@gmail.com>
2024-04-11 23:25:27 +0200

    Eliminacion de clases Usuario sin seguridad

--- src/main/java/com/pablodelacruz/coachify/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pablodelacruz.coachify.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.pablodelacruz.coachify.model.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    
-}
