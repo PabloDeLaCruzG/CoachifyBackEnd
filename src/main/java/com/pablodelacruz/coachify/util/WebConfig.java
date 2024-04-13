b57a0e96f7e6b7a52d70187f2d66614390f77922
PabloMonjiDev <pablo20044814@gmail.com>
2024-04-13 12:09:48 +0200

    Creacion WebCofig para CORS

---
+++ src/main/java/com/pablodelacruz/coachify/util/WebConfig.java
@@ -0,0 +1,18 @@
+package com.pablodelacruz.coachify.util;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
+        registry.addMapping("/**") // Permitimos solicitudes desde todas las rutas
+                .allowedOrigins("http://localhost:3000") // Permitimos solicitudes desde el frontend en el puerto 3000
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Permitimos los métodos HTTP
+                .allowedHeaders("*"); // Permitimos todos los encabezados
+    }
+}
+
