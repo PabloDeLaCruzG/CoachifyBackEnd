82bd628844aeed39be735b4299edefe8b4252d5b
PabloMonjiDev <pablo20044814@gmail.com>
2024-04-13 23:00:31 +0200

    Cambio en la configuracion del token(Funciona asi)

--- src/main/java/com/pablodelacruz/coachify/jwt/JwtService.java
+++ src/main/java/com/pablodelacruz/coachify/jwt/JwtService.java
@@ -11,61 +11,103 @@ import org.springframework.stereotype.Service;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 
 @Service
 public class JwtService {
 
-    private static final Key SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    private static final String SECRET_KEY="586E3272357538782F413F4428472B4B6250655368566B597033733676397924";
 
     public String getToken(UserDetails user) {
-        return getToken(new HashMap<>(), user);
+        try {
+            return getToken(new HashMap<>(), user);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public String getToken(Map<String, Object> extraClaims, UserDetails user) {
-        return Jwts
-            .builder()
-            .setClaims(extraClaims)
-            .setSubject(user.getUsername())
-            .setIssuedAt(new Date(System.currentTimeMillis()))
-            .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
-            .signWith(SECRET_KEY, SignatureAlgorithm.HS256)
-            .compact();
+        try {
+            return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(user.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
+                .signWith(getKey(), SignatureAlgorithm.HS256)
+                .compact();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Key getKey() {
+        try {
+            byte[] keyBytes=Decoders.BASE64.decode(SECRET_KEY);
+            return Keys.hmacShaKeyFor(keyBytes);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public String getUsernameFromToken(String token) {
-        return getClaim(token, Claims::getSubject);
+        try {
+            return getClaim(token, Claims::getSubject);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public boolean isTokenValid(String token, UserDetails userDetails) {
-        final String username=getUsernameFromToken(token);
-        return (username.equals(userDetails.getUsername())&& !isTokenExpired(token));
+        try {
+            final String username=getUsernameFromToken(token);
+            return (username.equals(userDetails.getUsername())&& !isTokenExpired(token));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private Claims getAllClaims(String token)
     {
-        return Jwts
-            .parserBuilder()
-            .setSigningKey(SECRET_KEY)
-            .build()
-            .parseClaimsJws(token)
-            .getBody();
+        try {
+            return Jwts
+                .parserBuilder()
+                .setSigningKey(getKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public <T> T getClaim(String token, Function<Claims,T> claimsResolver)
     {
-        final Claims claims=getAllClaims(token);
-        return claimsResolver.apply(claims);
+        try {
+            final Claims claims=getAllClaims(token);
+            return claimsResolver.apply(claims);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private Date getExpiration(String token)
     {
-        return getClaim(token, Claims::getExpiration);
+        try {
+            return getClaim(token, Claims::getExpiration);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private boolean isTokenExpired(String token)
     {
-        return getExpiration(token).before(new Date());
+        try {
+            return getExpiration(token).before(new Date());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
