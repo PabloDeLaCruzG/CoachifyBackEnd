e69a7a0d8d1eb42ae14a8ff66a9557aac04d3066
PabloMonjiDev <pablo20044814@gmail.com>
2024-05-12 12:52:19 +0200

    Fin

--- src/main/java/com/pablodelacruz/coachify/repository/HistorialMedicoRepository.java
+++ src/main/java/com/pablodelacruz/coachify/repository/HistorialMedicoRepository.java
@@ -4,8 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.pablodelacruz.coachify.model.HistorialMedico;
 
-import java.util.List;
-
 public interface HistorialMedicoRepository extends JpaRepository<HistorialMedico, Long>{
     
     HistorialMedico findByCliente_clienteID(Long clienteID);
