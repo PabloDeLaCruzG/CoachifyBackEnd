8c640a31c98c39fa106a4fd9d9d4470e6fce6fe8
PabloMonjiDev <pablo20044814@gmail.com>
2024-05-27 20:09:22 +0200

    Cambios de config

--- src/main/java/com/pablodelacruz/coachify/util/WebConfig.java
+++ src/main/java/com/pablodelacruz/coachify/util/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
         registry.addMapping("/**") // Permitimos solicitudes desde todas las rutas
-                .allowedOrigins("https://coachify-fitness.web.app") // Permitimos solicitudes desde el frontend en el puerto 3000
+                .allowedOrigins("https://coachify-fitness.web.app", "http://localhost:3000")// Permitimos solicitudes desde el frontend en el puerto 3000
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // Permitimos los métodos HTTP
                 .allowedHeaders("*")
                 .allowCredentials(true); // Permitimos todos los encabezados
