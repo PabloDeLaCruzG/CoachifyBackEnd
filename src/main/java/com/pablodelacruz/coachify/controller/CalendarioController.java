32acda02d0eb7a943088cac0813482be271f585a
PabloMonjiDev <pablo20044814@gmail.com>
2024-05-27 19:56:05 +0200

    Cambio de comentario

--- src/main/java/com/pablodelacruz/coachify/controller/CalendarioController.java
+++ src/main/java/com/pablodelacruz/coachify/controller/CalendarioController.java
@@ -8,23 +8,13 @@ import org.springframework.web.bind.annotation.*;
 import com.pablodelacruz.coachify.model.Calendario;
 import com.pablodelacruz.coachify.service.CalendarioService;
 
-/**
- * REST controller for managing Calendario entities.
- */
 @RestController
 @RequestMapping("/api/calendario")
 public class CalendarioController {
 
     @Autowired
-    CalendarioService calendarioService; // Inject the CalendarioService
+    CalendarioService calendarioService; 
 
-    /**
-     * GET /api/calendario : get all Calendario entities.
-     *
-     * @return the ResponseEntity with status 200 (OK) and the list of Calendario
-     *         entities, or status 500 (Internal Server Error) with the error
-     *         message.
-     */
     @GetMapping()
     public ResponseEntity<?> getAllCalendarios() {
         try {
@@ -34,15 +24,6 @@ public class CalendarioController {
         }
     }
 
-    /**
-     * GET /api/calendario/{calendarioID} : get the "calendarioID" Calendario
-     * entity.
-     *
-     * @param id the id of the Calendario entity
-     * @return the ResponseEntity with status 200 (OK) and the Calendario entity, or
-     *         status 404 (Not Found) if the Calendario is null, or status 500
-     *         (Internal Server Error) with the error message.
-     */
     @GetMapping("/{calendarioID}")
     public ResponseEntity<?> getCalendarioById(@PathVariable("calendarioID") Long id) {
         try {
@@ -52,15 +33,6 @@ public class CalendarioController {
         }
     }
 
-    /**
-     * POST /api/calendario : create a new Calendario entity.
-     *
-     * @param calendario the Calendario entity to create
-     * @return the ResponseEntity with status 201 (Created) and the newly created
-     *         Calendario entity, or status 400 (Bad Request) if the Calendario has
-     *         already an ID, or status 500 (Internal Server Error) with the error
-     *         message.
-     */
     @PostMapping()
     public ResponseEntity<?> createCalendario(@RequestBody Calendario calendario) {
         try {
@@ -70,16 +42,6 @@ public class CalendarioController {
         }
     }
 
-    /**
-     * PUT /api/calendario/{calendarioID} : updates the "calendarioID" Calendario
-     * entity.
-     *
-     * @param id                the id of the Calendario entity to update
-     * @param calendarioDetails the Calendario entity with the new data
-     * @return the ResponseEntity with status 200 (OK) and the updated Calendario
-     *         entity, or status 400 (Bad Request) if the Calendario entity is not
-     *         valid, or status 500 (Internal Server Error) with the error message.
-     */
     @PutMapping("/{calendarioID}")
     public ResponseEntity<?> updateCalendario(@PathVariable("calendarioID") String id,
             @RequestBody Calendario calendarioDetails) {
@@ -94,15 +56,6 @@ public class CalendarioController {
         }
     }
 
-    /**
-     * DELETE /api/calendario/{calendarioID} : delete the "calendarioID" Calendario
-     * entity.
-     *
-     * @param id the id of the Calendario entity to delete
-     * @return the ResponseEntity with status 200 (OK) and the deleted Calendario
-     *         entity, or status 400 (Bad Request) if the Calendario entity is not
-     *         valid, or status 500 (Internal Server Error) with the error message.
-     */
     @DeleteMapping("/{calendarioID}")
     public ResponseEntity<?> deleteCalendario(@PathVariable("calendarioID") Long id) {
         try {
